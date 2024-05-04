0e1eecb2404396f567f30005f7723507489222a0
ZRNOF <ZRNOF@protonmail.com>
2024-05-04 16:57:18 +0800

    init

---
+++ app/Counter.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { Button, Flex, Heading } from "@radix-ui/themes"
+import { motion, Variants } from "framer-motion"
+import useCounterStore from "./store"
+
+const Counter = () => {
+	const { count, inc, dec } = useCounterStore()
+
+	const transition = (delay: number): Variants => ({
+		initial: {
+			y: 100,
+			opacity: 0,
+		},
+		animate: {
+			y: 0,
+			opacity: 1,
+			transition: { delay },
+		},
+	})
+
+	return (
+		<Flex justify="center" align="center" direction="column" height="100dvh">
+			<motion.div {...transition(0)}>
+				<Heading size="9" mb="5">
+					{count}
+				</Heading>
+			</motion.div>
+			<Flex gap="5">
+				<motion.div {...transition(0.05)}>
+					<Button size="3" onClick={inc}>
+						Increase
+					</Button>
+				</motion.div>
+				<motion.div {...transition(0.1)}>
+					<Button size="3" onClick={dec}>
+						Decrease
+					</Button>
+				</motion.div>
+			</Flex>
+		</Flex>
+	)
+}
+
+export default Counter
---
+++ app/store.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand"
+
+interface CounterState {
+	count: number
+	inc: () => void
+	dec: () => void
+}
+
+const useCounterStore = create<CounterState>()((set) => ({
+	count: 0,
+	inc: () => set((state) => ({ count: state.count + 1 })),
+	dec: () => set((state) => ({ count: state.count - 1 })),
+}))
+
+export default useCounterStore
